95e8e90fba2f1ffb7180fe5e59286cd556290375
evilPaprika <ivil.paprika@gmail.com>
2017-10-29 23:06:54 +0500

    пофиксил выход за границы (еще раз)

--- src/app/util/UtilFunctions.java
+++ src/app/util/UtilFunctions.java
@@ -5,9 +5,9 @@ public class UtilFunctions {
 
     public static Point makePositionInBoundaries(Point position){
         if (position.x < 0 || position.x >= GameConsts.WIDTH)
-            position.x = mod((int)position.x, GameConsts.WIDTH);
+            position.x = mod((int) Math.floor(position.x), GameConsts.WIDTH);
         if (position.y < 0 || position.y >= GameConsts.HEIGHT)
-            position.y = mod((int)position.y, GameConsts.HEIGHT);
+            position.y = mod((int)Math.floor(position.y), GameConsts.HEIGHT);
         return position;
     }
 }
